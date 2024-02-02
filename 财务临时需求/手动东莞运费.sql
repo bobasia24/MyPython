b3de02a9cbe5086ffd32a761a7a0519f45624982
liyazhou <737694979@qq.com>
2024-02-02 17:05:47 +0800

    人工计算料工费与运费代码

--- 财务临时需求/手动东莞运费.sql
+++ 财务临时需求/手动东莞运费.sql
@@ -4,13 +4,11 @@ select c.序号,c.运输单号,c.总成本含税,a.总总成本含税,c.总成�
 from
 profit.手动导入11月临时表 c
 left join
-
 -- 获取合计成本
 (select 序号,运输单号,sum(总成本含税)总总成本含税  from
 profit.手动导入11月临时表
 group by 运输单号 )a
 on a.运输单号=c.运输单号
-
 left join
 -- 关联运费
 profit.手动导入11月运费 b
