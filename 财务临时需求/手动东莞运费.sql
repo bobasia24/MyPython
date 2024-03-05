c8643712a302ee60e91e6f3d685384d25744afc1
yaazooo <737694979@qq.com>
2024-03-05 16:24:20 +0800

    更新财务东莞运费脚本

--- 财务临时需求/手动东莞运费.sql
+++ 财务临时需求/手动东莞运费.sql
@@ -1,19 +1,28 @@
 
+select sum(总成本含税),sum(总总成本含税)  from (
 select c.序号,c.运输单号,c.总成本含税,a.总总成本含税,c.总成本含税/a.总总成本含税 as 比例,
 (c.总成本含税/a.总总成本含税)*b.含税金额 含税运费,(c.总成本含税/a.总总成本含税)*b.不含税金额 不含税运费,(c.总成本含税/a.总总成本含税)*b.税额 税额
 from
-profit.手动导入11月临时表 c
+profit.ods_2402运费临时表 c
 left join
 -- 获取合计成本
 (select 序号,运输单号,sum(总成本含税)总总成本含税  from
-profit.手动导入11月临时表
+profit.ods_2402运费临时表
 group by 运输单号 )a
 on a.运输单号=c.运输单号
 left join
 -- 关联运费
-profit.手动导入11月运费 b
+(select 单号,sum(含税金额) 含税金额,
+税率,
+sum(不含税金额) 不含税金额,
+sum(税额)税额 from
+profit.ods_2402运费 group by 单号) b
 on  a.运输单号 = b.单号
-order by c.序号
+order by c.序号)dd
+
+
+
+
 
 
 ------ 按成本均摊运费代码
