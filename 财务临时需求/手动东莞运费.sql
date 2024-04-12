7f47c0da7b7b7ebe103a4c039def8559b3a447a4
yaazooo <737694979@qq.com>
2024-04-12 10:36:43 +0800

    更新

--- 财务临时需求/手动东莞运费.sql
+++ 财务临时需求/手动东莞运费.sql
@@ -20,7 +20,24 @@ profit.ods_2402运费 group by 单号) b
 on  a.运输单号 = b.单号
 order by c.序号)dd
 
-
+select c.序号,c.运输单号,c.总成本含税,a.总总成本含税,c.总成本含税/a.总总成本含税 as 比例,
+(c.总成本含税/a.总总成本含税)*b.含税金额 含税运费,
+(c.总成本含税/a.总总成本含税)*b.含税金额 /1.06 不含税运费,
+(c.总成本含税/a.总总成本含税)*b.含税金额 -  (c.总成本含税/a.总总成本含税)*b.含税金额 /1.06  税额
+from
+profit.人工导入202402单品 c
+left join
+-- 获取合计成本
+(select 序号,运输单号,sum(总成本含税)总总成本含税  from
+profit.人工导入202402单品
+group by 运输单号 )a
+on a.运输单号=c.运输单号
+left join
+-- 关联运费
+(select 单号,sum(含税金额) 含税金额 from
+profit.人工导入202402运费汇总 group by 单号) b
+on  a.运输单号 = b.单号
+order by c.序号
 
 
 
