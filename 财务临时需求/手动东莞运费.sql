440a234a0a4ec085a23c4ffcca60d25d20e52119
yaazooo <737694979@qq.com>
2024-06-08 17:59:14 +0800

    更新运费

--- 财务临时需求/手动东莞运费.sql
+++ 财务临时需求/手动东莞运费.sql
@@ -1,44 +1,25 @@
-
-select sum(总成本含税),sum(总总成本含税)  from (
-select c.序号,c.运输单号,c.总成本含税,a.总总成本含税,c.总成本含税/a.总总成本含税 as 比例,
-(c.总成本含税/a.总总成本含税)*b.含税金额 含税运费,(c.总成本含税/a.总总成本含税)*b.不含税金额 不含税运费,(c.总成本含税/a.总总成本含税)*b.税额 税额
-from
-profit.ods_2402运费临时表 c
-left join
--- 获取合计成本
-(select 序号,运输单号,sum(总成本含税)总总成本含税  from
-profit.ods_2402运费临时表
-group by 运输单号 )a
-on a.运输单号=c.运输单号
-left join
--- 关联运费
-(select 单号,sum(含税金额) 含税金额,
-税率,
-sum(不含税金额) 不含税金额,
-sum(税额)税额 from
-profit.ods_2402运费 group by 单号) b
-on  a.运输单号 = b.单号
-order by c.序号)dd
+select sum(总成本含税),count(1)  from (
 
 select c.序号,c.运输单号,c.总成本含税,a.总总成本含税,c.总成本含税/a.总总成本含税 as 比例,
 (c.总成本含税/a.总总成本含税)*b.含税金额 含税运费,
 (c.总成本含税/a.总总成本含税)*b.含税金额 /1.06 不含税运费,
 (c.总成本含税/a.总总成本含税)*b.含税金额 -  (c.总成本含税/a.总总成本含税)*b.含税金额 /1.06  税额
 from
-profit.人工导入202402单品 c
+profit.人工导入2406运费单品表 c
 left join
 -- 获取合计成本
 (select 序号,运输单号,sum(总成本含税)总总成本含税  from
-profit.人工导入202402单品
+profit.人工导入2406运费单品表
 group by 运输单号 )a
 on a.运输单号=c.运输单号
 left join
 -- 关联运费
 (select 单号,sum(含税金额) 含税金额 from
-profit.人工导入202402运费汇总 group by 单号) b
+profit.人工导入2406运费底稿 group by 单号) b
 on  a.运输单号 = b.单号
 order by c.序号
 
+)ddd
 
 
 
